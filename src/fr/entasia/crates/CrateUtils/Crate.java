c9e02d064c347a5b71664f532fd71616b184a7ac
Stargeyt <max.bourgeois812@gmail.com>
2020-06-17 09:43:34 +0200

    a

---
+++ src/fr/entasia/crates/CrateUtils/Crate.java
@@ -0,0 +1,12 @@
+package fr.entasia.crates.CrateUtils;
+
+import org.bukkit.block.Block;
+
+import java.util.ArrayList;
+
+public class Crate {
+
+    public static ArrayList<CrateItem> items= new ArrayList<>();
+    public static String name;
+    public static Block block;
+}
---
+++ src/fr/entasia/crates/CrateUtils/CrateItem.java
@@ -0,0 +1,10 @@
+package fr.entasia.crates.CrateUtils;
+
+import org.bukkit.inventory.ItemStack;
+
+public class CrateItem {
+
+    public static ItemStack item;
+    public static int chance;
+    public static String name;
+}
