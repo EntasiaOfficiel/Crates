fc236bb663e23c82d27a0886fd0482889ef49e68
iTrooz_ <team.mortelgames@gmail.com>
2020-08-26 17:29:33 +0200

    fix

--- src/fr/entasia/crates/Listeners.java
+++ src/fr/entasia/crates/Listeners.java
@@ -21,15 +21,15 @@ public class Listeners implements Listener {
 		Player p = e.getPlayer();
 		Block b = e.getClickedBlock();
 		for(Map.Entry<Block, CrateType> entry : CratesAPI.crateLocs.entrySet()){
-			if(entry.getKey().equals(b)){
+			if(entry.getKey().equals(b)) {
 				e.setCancelled(true);
 				CrateType crateType = entry.getValue();
-					if(b.hasMetadata("opening")){
-						p.sendMessage("§cQuelqu'un est déjà en train d'ouvrir cette crate !");
-					}else{
-						if(crateType.removeKey(p))crateType.open(b,p);
-						else p.sendMessage("§cTu n'as pas la clé pour ouvrir cette crate !");
-					}
+				if (b.hasMetadata("opening")) {
+					p.sendMessage("§cQuelqu'un est déjà en train d'ouvrir cette crate !");
+				} else {
+					if (crateType.removeKey(p)) crateType.open(b, p);
+					else p.sendMessage("§cTu n'as pas la clé pour ouvrir cette crate !");
+				}
 				return;
 			}
 		}
